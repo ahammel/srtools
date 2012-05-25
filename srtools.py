9b6c4dc27a94a65c6ea08617e02baef9bdd0c5fe
Alex Hammel <ahammel87@gmail.com>
2012-05-25 13:46:36 -0700

    Added parse_sam_read function

--- srtools.py
+++ srtools.py
@@ -2,15 +2,24 @@ class Read():
     """A sam-format sequence read."""
     def __init__(self, qname, flag, rname, pos, mapq, cigar, rnext, pnext,
                  tlen, seq, qual, tags=[]):
-        self.qname = qname
-        self.flag = flag
-        self.rname = rname   
-        self.pos = pos   
-        self.mapq = mapq   
-        self.cigar = cigar   
-        self.rnext = rnext   
-        self.pnext = pnext  
-        self.tlen = tlen   
-        self.seq = seq   
-        self.qual = qual   
-        self.tags=[]
+        self.qname = str(qname)
+        self.flag = int(flag)
+        self.rname = str(rname)
+        self.pos = int(pos)
+        self.mapq = int(mapq)
+        self.cigar = str(cigar)
+        self.rnext = str(rnext)
+        self.pnext = int(pnext)
+        self.tlen = int(tlen)
+        self.seq = str(seq)
+        self.qual = str(qual)
+        self.tags = [str(x) for x in tags]
+
+
+def parse_sam_read(string):
+    """Takes a string in SAM -file format and returns a Read object."""
+    fields = string.strip().split()
+    return Read(fields[0], fields[1], fields[2], fields[3], fields[4],
+                fields[5], fields[6], fields[7], fields[8], fields[9],
+                fields[10], tags=fields[11:])
+
--- test_srtools.py
+++ test_srtools.py
@@ -1,26 +1,31 @@
 import pytest
 import srtools
 
+sam_str =  """
+SRR360147.1     77      *       0       0       *       *       0       0\
+ AGATGACGAAGAAGCTTGATCTCACGAANNNNNNNNTTNNCATCCNNNTNNTNNNNNNNNNNNNNNNNNNNNNNN\
+ HHHHHHHHHHHHHHHHHHHHHHHHH==@###############################################\
+ YT:Z:UP YF:Z:NS"""
+
+read = srtools.Read(
+        qname = "SRR360147.1",
+        flag = 77,
+        rname = '*',
+        pos = 0,
+        mapq = 0,
+        cigar = '*',
+        rnext = '*',
+        pnext = 0,
+        tlen = 0,
+        seq = ("AGATGACGAAGAAGCTTGATCTCACGAANNNNNNNNTTNNCATCCNNNT"
+               "NNTNNNNNNNNNNNNNNNNNNNNNNN"),
+        qual = ("HHHHHHHHHHHHHHHHHHHHHHHHH==@####################"
+                "###########################"),
+        tags = ["YT:Z:UP","YF:Z:NS"]
+        )
 
 def test_Read_class():
-    """Tests whether the Read.__init__ method is working correctly.
-
-    """
-    read = srtools.Read(
-            qname = "SRR360147.1",
-            flag = 77,
-            rname = '*',
-            pos = 0,
-            mapq = 0,
-            cigar = '*',
-            rnext = '*',
-            pnext = 0,
-            tlen = 0,
-            seq = ("AGATGACGAAGAAGCTTGATCTCACGAANNNNNNNNTTNNCATCCNNNT",
-                "NNTNNNNNNNNNNNNNNNNNNNNNNN"),
-            qual = ("HHHHHHHHHHHHHHHHHHHHHHHHH==@####################",
-                "###########################")
-            )
+    """Tests whether the Read.__init__ method is working correctly."""
     assert read.qname == "SRR360147.1"
     assert read.flag == 77
     assert read.rname == '*'
@@ -30,7 +35,23 @@ def test_Read_class():
     assert read.rnext == '*'
     assert read.pnext == 0
     assert read.tlen == 0
-    assert read.seq == ("AGATGACGAAGAAGCTTGATCTCACGAANNNNNNNNTTNNCATCCNNNT",
-                       "NNTNNNNNNNNNNNNNNNNNNNNNNN")
-    assert read.qual == ("HHHHHHHHHHHHHHHHHHHHHHHHH==@####################",
+    assert read.seq == ("AGATGACGAAGAAGCTTGATCTCACGAANNNNNNNNTTNNCATCCNNNT"
+                        "NNTNNNNNNNNNNNNNNNNNNNNNNN")
+    assert read.qual == ("HHHHHHHHHHHHHHHHHHHHHHHHH==@####################"
                          "###########################")
+    assert read.tags == ["YT:Z:UP","YF:Z:NS"]
+
+def test_parse_sam_read():
+    test_read = srtools.parse_sam_read(sam_str)
+    assert test_read.qname == read.qname
+    assert read.flag == test_read.flag 
+    assert read.rname == test_read.rname 
+    assert read.pos == test_read.pos 
+    assert read.mapq == test_read.mapq 
+    assert read.cigar == test_read.cigar 
+    assert read.rnext == test_read.rnext 
+    assert read.pnext == test_read.pnext 
+    assert read.tlen == test_read.tlen 
+    assert read.seq == test_read.seq 
+    assert read.qual == test_read.qual 
+    assert read.tags == test_read.tags 
