c0d119218fea32bd07a75594c449c35535fb5b49
Alex Hammel <ahammel87@gmail.com>
2012-10-09 14:14:16 -0700

    Debugged setup script

--- setup.py
+++ setup.py
@@ -5,12 +5,9 @@ setup(
         version='0.1.0',
         author='Alex J. Hammel',
         author_email='ahammel87@gmail.com',
-        packages=['srtools', 'srstats', 'sr_postgres'],
+        packages=['srtools'],
         url='http://pypi.python.org/pypi/srtools',
         license='LICENSE.txt',
         description='Tools for short-read genetic data.',
         long_description=open('README.txt').read(),
-        install_requires=[
-        "Python >= 3.2.3"
-        ],
      )
